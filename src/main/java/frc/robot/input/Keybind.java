a6c53edaa86b00f23997c02c11b6ca4e78b8bf45
Hugo Klepsch <hugo.klepsch@gmail.com>
2025-02-17 12:54:59 -0500

    Refactor to use enhanced switch statement.

--- src/main/java/frc/robot/input/Keybind.java
+++ src/main/java/frc/robot/input/Keybind.java
@@ -8,42 +8,45 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Keybind implements BooleanSupplier {
-    public enum Button {
-        A, B, X, Y, Start, Select,
-        LeftBumper, RightBumper,
-        LeftStick, RightStick;
+  public enum Button {
+    A, B, X, Y, Start, Select,
+    LeftBumper, RightBumper,
+    LeftStick, RightStick;
 
-        public Supplier<Boolean> getMethod(XboxController controller) {
-            switch (this) {
-                case A: return controller::getAButton;
-                case B: return controller::getBButton;
-                case X: return controller::getXButton;
-                case Y: return controller::getYButton;
-                case LeftBumper: return controller::getLeftBumperButton;
-                case RightBumper: return controller::getRightBumperButton;
-                case LeftStick: return controller::getLeftStickButton;
-                case RightStick: return controller::getRightStickButton;
-                case Start: return controller::getStartButton;
-                case Select: return controller::getBackButton;
-                default: return () -> false;
-            }
-        }
+    public Supplier<Boolean> getMethod(XboxController controller) {
+      return switch (this) {
+        case A -> controller::getAButton;
+        case B -> controller::getBButton;
+        case X -> controller::getXButton;
+        case Y -> controller::getYButton;
+        case LeftBumper -> controller::getLeftBumperButton;
+        case RightBumper -> controller::getRightBumperButton;
+        case LeftStick -> controller::getLeftStickButton;
+        case RightStick -> controller::getRightStickButton;
+        case Start -> controller::getStartButton;
+        case Select -> controller::getBackButton;
+      };
     }
-    XboxController hid;
-    public Button button;
-    public Keybind(CommandXboxController controller, Button button) {
-        this(controller.getHID(), button);
-    }
-    public Keybind(XboxController controller, Button button) {
-        hid = controller;
-        this.button = button;
-    }
-    @Override 
-    public boolean getAsBoolean() {
-        return button.getMethod(hid).get();
-    }
+  }
 
-    public Trigger trigger() {
-        return new Trigger(this);
-    }
+  XboxController hid;
+  public Button button;
+
+  public Keybind(CommandXboxController controller, Button button) {
+    this(controller.getHID(), button);
+  }
+
+  public Keybind(XboxController controller, Button button) {
+    hid = controller;
+    this.button = button;
+  }
+
+  @Override
+  public boolean getAsBoolean() {
+    return button.getMethod(hid).get();
+  }
+
+  public Trigger trigger() {
+    return new Trigger(this);
+  }
 }
