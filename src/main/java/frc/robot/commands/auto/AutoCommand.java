1d2ab0afafad38e25871e27d7b88adf5ff341fe5
Hugo Klepsch <hugo.klepsch@gmail.com>
2025-02-26 10:12:54 -0500

    Fix flipping to only flip if we're on red

--- src/main/java/frc/robot/commands/auto/AutoCommand.java
+++ src/main/java/frc/robot/commands/auto/AutoCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands.auto;
 
 import com.pathplanner.lib.util.FlippingUtil;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import java.util.List;
@@ -15,12 +16,23 @@ public abstract class AutoCommand extends SequentialCommandGroup {
 
   public List<Pose2d> getAllProperFlippedPathPoses() {
     List<Pose2d> rawPoses = getAllRawPathPoses();
+    boolean isOnRed = DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Red;
     return rawPoses.stream()
-            .map(FlippingUtil::flipFieldPose)
-            .collect(java.util.stream.Collectors.toList());
+        .map((Pose2d pose) -> {
+          if (isOnRed) {
+            return FlippingUtil.flipFieldPose(pose);
+          }
+          return pose;
+        })
+        .collect(java.util.stream.Collectors.toList());
   }
 
   public Pose2d getProperFlippedStartingPose() {
-    return FlippingUtil.flipFieldPose(getRawStartingPose());
+    boolean isOnRed = DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Red;
+    Pose2d rawStartingPose = getRawStartingPose();
+    if (isOnRed) {
+      return FlippingUtil.flipFieldPose(getRawStartingPose());
+    }
+    return rawStartingPose;
   }
 }
