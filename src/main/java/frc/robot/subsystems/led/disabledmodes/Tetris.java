5e503d1d479e5000465d41480d8b6444257aab52
bcrteammember <bcrteammember@gmail.com>
2025-03-06 17:00:03 -0500

    tetris!

--- src/main/java/frc/robot/subsystems/led/disabledmodes/Tetris.java
+++ src/main/java/frc/robot/subsystems/led/disabledmodes/Tetris.java
@@ -2,20 +2,22 @@ package frc.robot.subsystems.led.disabledmodes;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.led.LEDDrawer;
 import frc.robot.subsystems.led.Strip;
+
 // NOT DONE
 public class Tetris extends LEDDrawer {
 
   private final Color fg;
   private int stripIndex = 0;
   int placed = 0;
+  boolean slidingTime = false;
 
   public Tetris(LEDSubsystem susystem, AddressableLED ledStrip, AddressableLEDBuffer buffer,
-              Color fg
-  ) {
+      Color fg) {
     super(susystem, ledStrip, buffer);
     this.fg = fg;
   }
@@ -23,28 +25,71 @@ public class Tetris extends LEDDrawer {
   @Override
   public void draw() {
     Color bg = susystem.allianceColor;
-    susystem.setColour(susystem.fullStrip, bg);
+
+    SmartDashboard.putNumber("stripIndex", stripIndex);
+    SmartDashboard.putNumber("placed", placed);
+    SmartDashboard.putNumber("numleds", susystem.leftStrip.numLEDs);
+    SmartDashboard.putBoolean("slide", slidingTime);
+
+    if (slidingTime) {
+      susystem.setColour(susystem.fullStrip, fg);
+    } else {
+      susystem.setColour(susystem.fullStrip, bg);
+    }
 
     for (Strip strip : susystem.strips) {
-      susystem.safeSetLED(strip.end - strip.direction * stripIndex, fg);
-      for(int i = 0; i < placed; ++i) {
-        susystem.safeSetLED(strip.start + strip.direction + i, fg);
+      if (slidingTime) {
+        // susystem.safeSetLED(strip.start + strip.direction * stripIndex, bg);
+
+        for (int i = 0; i < placed; ++i) {
+          susystem.safeSetLED(strip.end - strip.direction * i, bg);
+        }
+
+      } else {
+        susystem.safeSetLED(strip.end - strip.direction * stripIndex, fg);
+
+        for (int i = 0; i < placed; ++i) {
+          susystem.safeSetLED(strip.start + strip.direction * i, fg);
+        }
+        // if (stripIndex < placed) {
+        // susystem.safeSetLED(strip.start + strip.direction * stripIndex, fg);
+        // }
+
+      }
+    }
+    
+    if (slidingTime) {
+      if (stripIndex < (susystem.leftStrip.numLEDs - 1)) {
+        placed++;
+      }
+
+      if (placed == susystem.leftStrip.numLEDs) {
+        slidingTime = false;
+        stripIndex = 0;
+        placed = 0;
+      }
+    } else {
+      if (stripIndex < (susystem.leftStrip.numLEDs - 1 - placed)) {
+        stripIndex++;
+      }
+
+      if (stripIndex == susystem.leftStrip.numLEDs - 1 - placed) {
+        stripIndex = 0;
+        placed++; // next one
+      }
+
+      if (placed == susystem.leftStrip.numLEDs - 1) {
+        slidingTime = true;
+        placed = 0;
+        stripIndex = 0;
       }
     }
 
-    if(stripIndex < (susystem.strips[0].numLEDs - 1 - placed)){
-      stripIndex++; 
-    }
-    
-    if(stripIndex == susystem.strips[0].numLEDs - placed){
-      stripIndex = 0;
-      placed++; // next one
-    }
   }
 
   @Override
   public int sleepInterval() {
-    return 30;
+    return 60;
   }
 
 }
